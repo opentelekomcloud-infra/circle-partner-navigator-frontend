1b8529f87aa9f2646bdaa169a0b6327ae53222de
tischrei <tino.schreiber@t-systems.com>
2024-02-07 08:48:25 +0000

    add Imprint and other legal information

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,24 +9,18 @@ function Footer() {
             <scale-telekom-footer-content>
                 <span slot="notice"> © Deutsche Telekom AG </span>
                 <ul slot="navigation">
-                <li>
-                    <Link href="#"> Imprint </Link>
-                </li>
-                <li>
-                    <Link href="#"> Data privacy </Link>
-                </li>
-                <li>
-                    <Link href="#"> Terms and conditions </Link>
-                </li>
-                <li>
-                    <Link href="#"> Consumer protection </Link>
-                </li>
-                <li>
-                    <Link href="#"> Product information sheet </Link>
-                </li>
-                <li>
-                    <Link href="#"> Youth protection </Link>
-                </li>
+                    <li>
+                        <Link href="https://www.open-telekom-cloud.com/_Resources/Persistent/1/d/5/f/1d5f960ae36cb3624ea1759fc5813d572f76e7cc/open-telekom-cloud-supplementary-conditions-commissioned-processing-personal-data.pdf">Commissioned Processing</Link>
+                    </li>
+                    <li>
+                        <Link href="https://www.open-telekom-cloud.com/en/disclaimer-of-liability">Disclaimer of liability</Link>
+                    </li>
+                    <li>
+                        <Link href="https://www.open-telekom-cloud.com/en/data-protection">Data privacy</Link>
+                    </li>
+                    <li>
+                        <Link href="https://www.open-telekom-cloud.com/en/imprint">Imprint</Link>
+                    </li>
                 </ul>
             </scale-telekom-footer-content>
         </scale-telekom-footer>
