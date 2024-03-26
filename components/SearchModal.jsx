503e8fc873de7a63ecb65f974e1bcfda961f4521
Sebastian Gode <sebastian.gode@telekom.de>
2024-03-26 10:13:26 +0000

    Added modal

---
+++ components/SearchModal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchModal({props, locale}) {
+  return (
+    <scale-modal heading="Today is your lucky day" size="large" id="SearchModal">
+        <p>Hello. Welcome. What a pleasure it is to have you.</p>
+    </scale-modal>
+  );
+}
+
+export default SearchModal;
