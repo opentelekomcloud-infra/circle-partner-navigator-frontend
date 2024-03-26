2b454df9a457ff20913b8810fe5811d9bd219923
tischrei <tino.schreiber@t-systems.com>
2024-03-26 13:58:25 +0000

    improve search functionality

--- components/SearchModal.jsx
+++ components/SearchModal.jsx
@@ -1,5 +1,70 @@
 import React from "react";
 
+import { ScaleTextField } from '@telekom/scale-components-react';
+
+var timerID = 0;
+
+// Build the request for searching
+async function searchRequest(val, request_size, highlight_size) {
+
+    // Default request without filtering
+    const request_query = {
+        "from" : 0, "size" : request_size,
+        "_source": ["highlight", "current_page_name", "title", "base_url", "doc_url", "doc_type", "doc_title", "service_title"],
+        "query": {
+            "multi_match": {
+              "query": val,
+              "type": "bool_prefix",
+              "operator": "and",
+              "fields": [ "body", "title^2" ]
+            }
+        },
+        "highlight": {
+            "number_of_fragments": 1,
+            "fragment_size":highlight_size,
+            "pre_tags": [
+                "<span style='color: var(--telekom-color-text-and-icon-primary-standard)'>"
+            ],
+            "post_tags": [
+                "</span>"
+            ],
+            "fields":{
+                "body": {},
+                "title": {}
+            },
+            "require_field_match" : false
+        }
+    };
+
+
+    let url = `${search_url}${search_environment}-*/_search`
+
+    let response = await postRequest(url, request_query)
+
+    return response
+};
+
+
+// TIMER WHICH STARTS THE SEARCH RESULT LIST AFTER TIMEOUT HAS BEEN REACHED
+function timer(el) {
+    timerID = setTimeout(async () => {
+        if (el.value) {
+            let response = await searchRequest(document.getElementById('searchbox').value, 100, 300);
+            createMainResult(response)
+        } else {
+            deleteResults();
+        };
+    }, 250);
+};
+
+// FUNCTION WHICH STARTS THE TIMER EVENT AFTER THE KEY UP EVENT
+const getSearchResults = async () => {
+    clearTimeout(timerID);
+    const el = document.getElementById('searchbox');
+    timer(el);
+};
+
+
 function SearchModal({props, lang}) {
 
     let heading = ""
@@ -16,7 +81,8 @@ function SearchModal({props, lang}) {
 
     return (
         <scale-modal heading={heading} size="large" id="SearchModal">
-            <p>{description}</p>
+            <ScaleTextField id="searchbox" label={description} onScaleChange={getSearchResults}>
+            </ScaleTextField>
         </scale-modal>
     );
 }
