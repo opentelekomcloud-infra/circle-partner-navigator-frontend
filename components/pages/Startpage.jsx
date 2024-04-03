12164c21f49c9452b9dbf1cbea5b8f15328ea8c6
tischrei <tino.schreiber@t-systems.com>
2024-04-03 08:45:30 +0000

    add new CPN logo and fix image behavior

--- components/pages/Startpage.jsx
+++ components/pages/Startpage.jsx
@@ -27,22 +27,28 @@ export default async function Startpage({ props }) {
         iconAlt: otcIcon["attributes"]["name"]
     }
 
-    const introMedia = webAssets.find(asset => asset.attributes.name === 'circle-partner-tp')
+    const introMedia = webAssets.find(asset => asset.attributes.name === 'cpn-logo-light')
     const introMediaHash = introMedia["attributes"]["media"]["data"]["attributes"]["hash"]
     const introMediaExt = introMedia["attributes"]["media"]["data"]["attributes"]["ext"]
+    
+    const introMediaDark = webAssets.find(asset => asset.attributes.name === 'cpn-logo-dark')
+    const introMediaDarkHash = introMediaDark["attributes"]["media"]["data"]["attributes"]["hash"]
+    const introMediaDarkExt = introMediaDark["attributes"]["media"]["data"]["attributes"]["ext"]
 
     let intro = {}
     if (props["locale"] === "en") {
         intro = {
             image: `${introMediaHash}${introMediaExt}`,
-            img_height: "517px",
+            image_dark: `${introMediaDarkHash}${introMediaDarkExt}`,
+            img_height: "143px",
             headline: "Discover the powerful services of our partners",
             text: ["Many start-ups and cloud service providers already work together with us an partners, using our technology and infrastructure for their cloud projects. This is a testament to their trust in the Open Telekom Cloud and our great partnership.", "You can start using these services today to improve your IT systems or your company's communications and benefit from the advantages of the GDPR-compliant European cloud."]
         }
     } else if (props["locale"] === "de-DE") {
         intro = {
             image: `${introMediaHash}${introMediaExt}`,
-            img_height: "517px",
+            image_dark: `${introMediaDarkHash}${introMediaDarkExt}`,
+            img_height: "143px",
             headline: "Entdecken Sie die leistungsstarken Services unserer Partner",
             text: ["Viele Start-ups und Cloud-Service-Anbieter arbeiten bereits mit uns als Partner zusammen und nutzen unsere Technologie und Infrastruktur für ihre Cloud-Produkte. Dies ist ein Beweis für ihr Vertrauen in die Open Telekom Cloud und unsere großartige Partnerschaft.", "Sie können diese Dienste schon heute einsetzen, um Ihre IT-Systeme oder die Kommunikation Ihres Unternehmens zu verbessern und von den Vorteilen der DSGVO-konformen europäischen Cloud zu profitieren."]
         }
