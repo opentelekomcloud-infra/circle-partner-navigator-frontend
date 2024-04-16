6a4a89391529aaecc186664fe7276eed2f192a36
tischrei <tino.schreiber@t-systems.com>
2024-04-16 13:12:32 +0000

    small update

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -4,14 +4,12 @@ import React from "react";
 function SearchResults({results}) {
     return (
         <div>
-            {console.log(results)}
+            {/* {console.log(results)} */}
             {results ? results.map((result, index) => (
                 <div key={index}>
                     <div style={{fontSize: "1.5rem", fontWeight: 'bold'}}>{result["_source"]["attributes"]["overview_headline"]}</div>
                     <div style={{color: 'blue'}}>https://dev.schreiber-ling.de/?folder=/home/ubuntu/workspace</div>
-                    <div>
-                        {result["highlight"][Object.keys(result["highlight"])[0]]}
-                    </div>
+                    <div dangerouslySetInnerHTML={{ __html: result["highlight"][Object.keys(result["highlight"])[0]] }}></div>
                 </div>                
             )) : null}
         </div>
