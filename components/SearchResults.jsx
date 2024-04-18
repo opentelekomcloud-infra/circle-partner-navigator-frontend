85c8e8d36edc264188ffaff8ddc33fc018b8d0b2
tischrei <tino.schreiber@t-systems.com>
2024-04-18 14:01:16 +0000

    fix visuals and add function to close modal

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -4,13 +4,20 @@ import Link from "next/link";
 
 import styles from '@/styles/SearchResults.module.css';
 
+
+const closeSearchModal = () => {
+    const modal = document.getElementById('SearchModal');
+    modal.opened = false;
+}
+
+
 function SearchResults({results, locale}) {
     return (
         <div className={styles.results_container}>
             {/* {console.log(results)} */}
             {results ? results.map((result, index) => (
                 <div  key={index} className={styles.result}>
-                    <Link href={`/${locale}/partners/${result["_id"]}`} className={styles.atag}>
+                    <Link href={`/${locale}/partners/${result["_id"]}`} className={styles.atag} onClick={closeSearchModal}>
                         <div className={styles.header}>{result["_source"]["attributes"]["overview_headline"]}</div>
                         <div className={styles.link}>{`/${locale}/partners/${result["_id"]}`}</div>
                         <div className={styles.highlight} dangerouslySetInnerHTML={{ __html: result["highlight"][Object.keys(result["highlight"])[0]] }}></div>
