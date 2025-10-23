0d2a1b7c5dc1946701609597f19df31c9a9297af
Tino Schr <tino.schreiber@t-systems.com>
2025-10-23 15:44:43 +0200

    fix search (#64)
    fix search
    
    Reviewed-by: SebastianGode

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -11,29 +11,48 @@ const closeSearchModal = () => {
 }
 
 
-function SearchResults({results, locale, empty}) {
+function SearchResults({ results, locale, empty }) {
     const noResultsMessage =
         locale === "de" ? "Keine Ergebnisse gefunden." : "No results found.";
+
     return (
         <div className={styles.results_container}>
             {/* {console.log(results)} */}
-            { !empty ? (
-                results && results.length > 0 ? results.map((result, index) => (
-                    <div  key={index} className={styles.result}>
-                        <Link href={`/${locale}/partners/${result["_id"]}`} className={styles.atag} onClick={closeSearchModal}>
-                            <div className={styles.header}>{result["_source"]["overview_headline"]}</div>
-                            <div className={styles.link}>{`/${locale}/partners/${result["_id"]}`}</div>
-                            <div className={styles.highlight} dangerouslySetInnerHTML={{ __html: result["highlight"][Object.keys(result["highlight"])[0]] }}></div>
-                        </Link>
-                    </div>
-                )) : (
+            {!empty ? (
+                results && results.length > 0 ? (
+                    results.map((result, index) => {
+                        const partnerId = result._source?.attributes?.partner_id;
+                        const headline = result._source?.attributes?.overview_headline;
+                        const highlightKey = Object.keys(result.highlight || {})[0];
+                        const highlightHtml = result.highlight?.[highlightKey] || "";
+
+                        if (!partnerId) return null; // falls kein partner_id vorhanden ist
+
+                        return (
+                            <div key={index} className={styles.result}>
+                                <Link
+                                    href={`/${locale}/partners/${partnerId}`}
+                                    className={styles.atag}
+                                    onClick={closeSearchModal}
+                                >
+                                    <div className={styles.header}>{headline}</div>
+                                    <div className={styles.link}>
+                                        {`/${locale}/partners/${partnerId}`}
+                                    </div>
+                                    <div
+                                        className={styles.highlight}
+                                        dangerouslySetInnerHTML={{ __html: highlightHtml }}
+                                    ></div>
+                                </Link>
+                            </div>
+                        );
+                    })
+                ) : (
                     <div>{noResultsMessage}</div>
                 )
-            ) : (
-                null
-            )}
+            ) : null}
         </div>
-    )
+    );
 }
 
 export default SearchResults;
