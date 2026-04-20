0fadc44d527942363f843c8355da9cff8e5aae89
tischrei <tino.schreiber@t-systems.com>
2026-04-20 08:31:07 +0000

    add new preprod sitekey

--- components/PartnerContactForm.jsx
+++ components/PartnerContactForm.jsx
@@ -8,7 +8,7 @@ import { ScaleButton, ScaleTextarea } from '@telekom/scale-components-react';
 
 // Settings for correct Captcha workflow
 const captchaSettings = {
-    captcha_sitekey: "8a7ff80d89",
+    captcha_sitekey: "f50d99b8cf",
     captcha_url: "https://cap.eco-preprod.tsi-dev.otc-service.com/"
 }
 
